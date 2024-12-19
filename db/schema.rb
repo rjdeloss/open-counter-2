f6cdecc45e15d71b51a197798266a41ae593271f
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-12-19 09:55:35 -0500

    add the generated schema that was forgotten

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_19_005142) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "cuisines", force: :cascade do |t|
+    t.string "cuisine", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cuisine"], name: "index_cuisines_on_cuisine"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_favorites_on_restaurant_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "start_time", null: false
+    t.integer "party_size", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "restaurant_cuisines", force: :cascade do |t|
+    t.integer "restaurant_id", null: false
+    t.integer "cuisine_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "city", null: false
+    t.string "zip", null: false
+    t.float "lat", null: false
+    t.float "lon", null: false
+    t.string "phone_number", null: false
+    t.string "price_range", null: false
+    t.text "description", null: false
+    t.integer "cuisine_id"
+    t.time "open_time", null: false
+    t.time "close_time", null: false
+    t.integer "capacity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["city"], name: "index_restaurants_on_city"
+    t.index ["price_range"], name: "index_restaurants_on_price_range"
+    t.index ["zip"], name: "index_restaurants_on_zip"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.integer "overall_rating"
+    t.integer "food_rating"
+    t.integer "service_rating"
+    t.integer "ambiance_rating"
+    t.integer "value_rating"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+  end
 end
