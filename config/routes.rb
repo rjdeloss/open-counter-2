1387052f19739af01977e12b4de2afd266db6bc4
Rafiluis De Los Santos <29210707+rjdeloss@users.noreply.github.com>
2025-01-03 11:46:32 -0500

    fix linting issues in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  namespace :api, defaults: {format: :json} do 
+  namespace :api, defaults: { format: :json } do
     resource :users, only[:create]
   end
-  
 end
