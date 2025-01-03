e37f5b488dd19befe39b9bef2f321e45ed18e64a
Rafiluis De Los Santos <29210707+rjdeloss@users.noreply.github.com>
2025-01-03 11:07:32 -0500

    add route for user creation

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api, defaults: {format: :json} do 
+    resource :users, only[:create]
+  end
+  
 end
