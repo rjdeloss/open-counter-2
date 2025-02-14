23d02e0386550310183c554bc60199837b97e9cf
Rafiluis De Los Santos <29210707+rjdeloss@users.noreply.github.com>
2025-02-14 11:05:50 -0500

    create store and rootReducer boilerplate

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,12 @@
+import type { Action, ThunkAction } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducers/rootReducer'
+
+export const store = configureStore({
+  reducer: rootReducer
+})
+
+export type AppStore = typeof store
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState, unknown, Action>
